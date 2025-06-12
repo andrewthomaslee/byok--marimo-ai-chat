97786c11c1a054d1a7ce310204ea890fec186483
Andrew Lee <andrewthomaslee.business@gmail.com>
2025-06-12 18:19:06 -0500

    tooltip not scale-0

--- app.py
+++ app.py
@@ -9,25 +9,16 @@ app = marimo.App(
 
 with app.setup:
     import marimo as mo
-    from mohtml import div, p, h1, bootstrap_css, h5, i, a, script,img
+    from mohtml import div, p, h1, bootstrap_css, h5, i, a, script ,img ,span
     from typing import Literal, Union
 
 
 @app.cell
 def _():
     txt = "The quick brown fox jumped over the lazy dog. 🐶"
-    return (txt,)
-
-
-@app.cell
-def _():
     style = "text-red-800 text-shadow-cyan-500 font-extrabold text-center text-shadow-md"
-    return (style,)
-
-
-@app.cell
-def _(style, txt):
     div(txt,klass=style)
+    None
     return
 
 
@@ -44,7 +35,7 @@ def _():
 
 
 @app.function
-def side_bar(elms:list)->div:
+def side_bar(elms:list[div])->div:
     return (
         div(*elms,
            klass="fixed top-0 left-0 h-screen w-16 flex flex-col bg-gray-900 text-white shadow-lg"
@@ -68,22 +59,46 @@ def _():
 
 @app.cell
 def _(my_icons):
-    def fetch_icon(icon:str,klass:str="my-icon",*,kwargs:dict=None)->mo.icon:
+    def fetch_icon(icon:str,*,kwargs:dict=None)->mo.icon:
         icon = my_icons.get(icon,my_icons["missing"])
         elm = mo.icon(icon_name=icon) if kwargs is None else mo.icon(icon_name=icon,**kwargs)
-        return div(elm,klass=klass)
+        return elm
     return (fetch_icon,)
 
 
 @app.cell
 def _(fetch_icon, my_icons):
     side_icon_list = [
-        fetch_icon(i,"side-bar-icon",kwargs={'size':25})
+        fetch_icon(i,kwargs={'size':25})
         for i in my_icons.keys()
     ]
 
-    side_bar(side_icon_list)
+    side_bar_icons_wrapped = [
+        sidebar_icon(elm)
+        for elm in side_icon_list
+    ]
+
+    side_bar(side_bar_icons_wrapped)
     return
+
+
+@app.function
+def sidebar_icon(icon:mo.icon,text='tooltip 💡'):
+    return div(
+        icon,
+        span(
+            text,
+            klass="sidebar-tooltip group-hover:scale-100"
+        ),
+        klass="sidebar-icon group"
+    )
+
+
+@app.function
+def sidebar_button(icon:mo.icon):
+    return mo.ui.button(
+        label=icon.text
+    )
 
 
 if __name__ == "__main__":
